30eba2ed3f5e4ef8b7b79299a1f5939231ad89d8
JorgeHSLA <jorgesierralaiton@gmail.com>
2025-08-07 21:54:14 -0500

    se agrega un adelanto de tienda

---
+++ picantito/src/main/java/com/picantito/picantito/controllers/TiendaController.java
@@ -0,0 +1,17 @@
+package com.picantito.picantito.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class TiendaController {
+
+
+    
+    @GetMapping("/tienda")
+    public String tienda() {
+        return "tienda";
+    }
+}
