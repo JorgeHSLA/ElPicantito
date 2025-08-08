a0db94fdfeb95eaa63b44f7c716369e87bd5d484
JorgeHSLA <jorgesierralaiton@gmail.com>
2025-08-07 21:39:12 -0500

    Agregamos SPRINGBOOT

---
+++ picantito/src/main/java/com/picantito/picantito/PicantitoApplication.java
@@ -0,0 +1,13 @@
+package com.picantito.picantito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicantitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicantitoApplication.class, args);
+	}
+
+}
