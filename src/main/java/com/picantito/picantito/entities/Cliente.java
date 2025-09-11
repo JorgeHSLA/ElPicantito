6079e02f18c3a20373745561c0b1000e431be26f
diegostrg <munoz.juand@javeriana.edu.co>
2025-09-11 11:07:01 -0500

    Cambio de .Java a .java en AdminController, creacion de entidades y repositorios por entidad.

--- src/main/java/com/picantito/picantito/entities/Cliente.java
+++ src/main/java/com/picantito/picantito/entities/Cliente.java
@@ -1,10 +1,43 @@
 package com.picantito.picantito.entities;
 
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
-
+@Entity
+@Table(name = "clientes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(nullable = false)
+    private String nombreCompleto;
+    
+    @Column(unique = true, nullable = false)
+    private String nombreUsuario;
+    
+    @Column(nullable = false)
+    private String telefono;
+    
+    @Column(unique = true, nullable = false)
+    private String correo;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Column(nullable = false)
+    private String direccion;
+    
+    private String preferenciasEntrega;
 }
