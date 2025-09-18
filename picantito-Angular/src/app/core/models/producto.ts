845482f4999a87225967d20e8efa882c976b0aa6
Javier Aldana <al_javier@javeriana.edu.co>
2025-09-18 11:51:19 -0500

    estructura carpetas y archivos

--- picantito-Angular/src/app/core/models/producto.ts
+++ picantito-Angular/src/app/core/models/producto.ts
@@ -1,3 +1,5 @@
+import { Adicional } from './adicional';
+
 export interface Producto {
   id?: number;
   nombre: string;
---
+++ picantito-Angular/src/app/features/admin/adicionales/adicionales.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionales',
+  imports: [],
+  templateUrl: './adicionales.html',
+  styleUrl: './adicionales.css'
+})
+export class Adicionales {
+
+}
