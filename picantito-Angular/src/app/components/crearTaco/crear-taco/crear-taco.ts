0c969f0c0ab9f47b59237f37460e96db89df35b5
JorgeHSLA <jorgesierralaiton@gmail.com>
2025-09-19 11:30:13 -0500

    mejorando

---
+++ picantito-Angular/src/app/components/crearTaco/crear-taco/crear-taco.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-taco',
+  imports: [],
+  templateUrl: './crear-taco.html',
+  styleUrl: './crear-taco.css'
+})
+export class CrearTaco {
+
+}
