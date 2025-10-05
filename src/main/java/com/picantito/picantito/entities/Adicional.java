038403d82078fd5b2a04b7cffae70c2c9d546ec3
JorgeHSLA <jorgesierralaiton@gmail.com>
2025-10-04 21:32:12 -0500

    feat: quite estorbos

--- src/main/java/com/picantito/picantito/entities/Adicional.java
+++ src/main/java/com/picantito/picantito/entities/Adicional.java
@@ -2,7 +2,6 @@ package com.picantito.picantito.entities;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
@@ -13,7 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -42,48 +40,6 @@ public class Adicional {
     @JsonIgnore
     @OneToMany(mappedBy = "adicional", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<PedidoProductoAdicional> pedidoProductoAdicionales = new ArrayList<>();
-
-    @JsonIgnore
-    @Transient
-    public List<Pedido> getPedidos() {
-        return this.pedidoProductoAdicionales.stream()
-            .map(ppa -> ppa.getPedidoProducto().getPedido())
-            .distinct()
-            .toList();
-    }
-
-    public void setPedidos(List<Pedido> pedidos) {
-        this.pedidoProductoAdicionales.clear();
-        for (Pedido pedido : pedidos) {
-            PedidoProducto pedidoProducto = new PedidoProducto();
-            pedidoProducto.setPedido(pedido);
-
-            PedidoProductoAdicional ppa = new PedidoProductoAdicional();
-            ppa.setPedidoProducto(pedidoProducto);
-            ppa.setAdicional(this);
-            ppa.setCantidadAdicional(1); // valor por defecto
-
-            this.pedidoProductoAdicionales.add(ppa);
-        }
-    }
-    @Transient
-    public List<Producto> getProductos() {
-        return this.productoAdicionales.stream()
-            .map(ProductoAdicional::getProducto)
-            .distinct()
-            .collect(Collectors.toCollection(ArrayList::new)); // ✅ mutable
-    }
-
-
-    public void setProductos(List<Producto> productos) {
-        this.productoAdicionales.clear();
-        for (Producto producto : productos) {
-            ProductoAdicional pa = new ProductoAdicional();
-            pa.setProducto(producto);
-            pa.setAdicional(this);
-            this.productoAdicionales.add(pa);
-        }
-    }
 
 
 }
