47ab74d8f671ac6718e87f70fd8cb3ee4972d1cc
diegostrg <munoz.juand@javeriana.edu.co>
2025-10-06 23:46:51 -0500

    La vaina de javi q no compilaba, osea los atributos en usuario

--- picantito-Angular/src/app/models/usuario.ts
+++ picantito-Angular/src/app/models/usuario.ts
@@ -5,7 +5,10 @@ export interface Usuario {
   telefono?: string;
   correo?: string;
   contrasenia?: string;
+  password?: string;
   rol?: string;
   estado?: string;
   activo?: boolean;
+  fechaRegistro?: string;
+  pedidosRealizados?: number;
 }
